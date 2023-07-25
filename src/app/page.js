8b3c175e0f00d38179a4ec62231d4ba22a43ed7e
David Lorenz <david@wahnsinn.design>
2023-07-25 09:48:24 +0200

    changed compose

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      New Version. DigiChange, changed page only Downtime.
+      New Version. XDeploy, changed page only Downtime.
     </main>
   );
 }
