c3fb4c11834372a3000f36a9829a3c5f0a9081d6
David Lorenz <david@wahnsinn.design>
2023-07-25 10:13:15 +0200

    push

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      New Version23. WH maybe, changed page only Downtime.
+      New Version234. WH maybe, changed page only Downtime.
     </main>
   );
 }
